5c573fdcef767f5d22c2879aef0670d66a9e843d
VAVA12345 <muhammedsirajudeen29@gmail.com>
2022-05-06 00:45:18 +0530

    added proper database integration

--- routes/Database/database.js
+++ routes/Database/database.js
@@ -1,12 +1,27 @@
-function FetchFromdb(){
+var database_data
+function FetchFromdb(hey,name){
 
 const mongo=require('mongodb');
 const MongoClient=mongo.MongoClient;
-const url='mongodb://localhost:27017';
-MongoClient.connect(url,{useNewUrlParser:true},(err,client)=>{
-    if(!err){
-        console.log("connection successful")
-    }
-});
+const url='mongodb://127.0.0.1:27017';
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+
+ 
+    const db = client.db("Ecommerce");
+
+    db.collection('Authentication').find(name).toArray().then((docs) => {
+
+        hey(docs)
+        
+        });
+    });
+    
 }
+
+
 module.exports.FetchFromdb=FetchFromdb;
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var con=require('./Database/database')
+var id=1;
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login');
 });
-router.post('/submit',(req,res)=>{
-  let username=req.body.username
-  let password=req.body.password
-  if(username==='vava'){
-    res.render('index',{login:true,user:false})
-    con.FetchFromdb()
+router.post('/submit-login',async function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    var name={"name":username}
+
+    con.FetchFromdb(function hey(docs) {
+        console.log(docs)
+        res.render('index',{user:true,login:true})
+   
+        
+
+    },name)
+
+
+  });
+
+router.get('/CreateAccount', function(req, res, next) {
+  res.render('CreateAccount');
+});
+router.post('/submit-logout',(req,res,next)=>{
+  let username_create=req.body.username
+  let password_create=req.body.password
+  console.log(username_create)
+  console.log(password_create)
+  if (username_create!=" "){
+    console.log("\n success\n")
   }else{
-    res.render('index',{login:true})
+    print("the entered value is null")
   }
+  res.render('login')
 });
+
 
 module.exports = router;
